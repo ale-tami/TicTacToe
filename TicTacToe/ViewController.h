fb8db0da52a5cc5e32c24b1080d5ebac14470af2
ale-tami <alejandrotami@gmail.com>
2014-07-25 08:57:49 -0300

    first push

---
+++ TicTacToe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TicTacToe
+//
+//  Created by Alejandro Tami on 24/07/14.
+//  Copyright (c) 2014 Alejandro Tami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TicTacToe/ViewController.m
@@ -0,0 +1,51 @@
+//
+//  ViewController.m
+//  TicTacToe
+//
+//  Created by Alejandro Tami on 24/07/14.
+//  Copyright (c) 2014 Alejandro Tami. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *myLabelOne;
+@property (weak, nonatomic) IBOutlet UILabel *myLabelTwo;
+@property (weak, nonatomic) IBOutlet UILabel *myLabelThree;
+@property (weak, nonatomic) IBOutlet UILabel *myLabelFour;
+@property (weak, nonatomic) IBOutlet UILabel *myLabelFive;
+@property (weak, nonatomic) IBOutlet UILabel *myLabelSix;
+@property (weak, nonatomic) IBOutlet UILabel *myLabelSeven;
+@property (weak, nonatomic) IBOutlet UILabel *myLabelEight;
+@property (weak, nonatomic) IBOutlet UILabel *myLabelNine;
+@property (weak, nonatomic) IBOutlet UILabel *whichPlayerLabel;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void) findLabelUsingPoint:(CGPoint)point
+{
+    
+}
+
+- (IBAction)onLabelTapped:(UITapGestureRecognizer *)sender
+{
+    [sender locationInView:self.view];
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
